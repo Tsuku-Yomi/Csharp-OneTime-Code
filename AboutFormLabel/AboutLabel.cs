a25e1f25b1d2b35d38ea5e06dd811d77a61f5a99
Tsuku-Yomi <523896791@qq.com>
2022-01-02 22:55:56 +0800

    添加项目文件。

---
+++ AboutFormLabel/AboutLabel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ATP.AboutFormLabel {
+    public partial class AboutLabel : UserControl {
+        public AboutLabel() {
+            InitializeComponent();
+        }
+    }
+}
